19aee5bc85b7cf93a361dd4d7468cf372ed13730
SSen <i@ssen.name>
2020-03-02 13:38:42 +0900

    - update eslint config - add prettier config - remove tslint config - update storybook config (for CSF)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,3 @@
 import { configure } from '@storybook/react';
 
-configure(() => {
-  const req = require.context('../src', true, /\.stories\.(js|jsx)$/);
-  req.keys().forEach(req);
-}, module);
+configure(require.context('../src', true, /stories\.(js|jsx)$/), module);
--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export function Sample({text}) {
-  return (
-    <div>{text}</div>
-  );
+export function Sample({ text }) {
+  return <div>{text}</div>;
 }
